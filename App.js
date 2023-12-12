86ed979ac590866f7777b600187110f3b511ba34
Colton Pinner <coltonpinner24@gmail.com>
2023-12-12 09:55:33 -0500

    // Version 1.0.0

--- App.js
+++ App.js
@@ -1,20 +1,159 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StatusBar, TextInput, TouchableOpacity, StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+// Sign In Screen
+function SignInScreen({ navigation }) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignIn = () => {
+    // Add your sign-in logic here
+    console.log('Sign In - Username:', username);
+    console.log('Sign In - Password:', password);
+    // You can implement your authentication logic (e.g., API calls) here
+  };
+
+  return (
+    <View style={styles.container}>
+      
+
+      {/* Username Input */}
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        placeholderTextColor="#A9A9A9"
+        onChangeText={(text) => setUsername(text)}
+        value={username}
+      />
+
+      {/* Password Input */}
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        placeholderTextColor="#A9A9A9"
+        secureTextEntry
+        onChangeText={(text) => setPassword(text)}
+        value={password}
+      />
+
+      {/* Sign In Button */}
+      <TouchableOpacity style={styles.button} onPress={handleSignIn}>
+        <Text style={styles.buttonText}>Sign In</Text>
+      </TouchableOpacity>
+
+      {/* Navigate to Sign Up */}
+      <TouchableOpacity onPress={() => navigation.navigate('SignUp')}>
+        <Text style={styles.linkText}>Don't have an account? Sign Up</Text>
+      </TouchableOpacity>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+// Sign Up Screen
+function SignUpScreen() {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignUp = () => {
+    // Add your sign-up logic here
+    console.log('Sign Up - Username:', username);
+    console.log('Sign Up - Email:', email);
+    console.log('Sign Up - Password:', password);
+    // You can implement your registration logic (e.g., API calls) here
+  };
+
+  return (
+    <View style={styles.container}>
+
+      {/* Username Input */}
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        placeholderTextColor="#A9A9A9"
+        onChangeText={(text) => setUsername(text)}
+        value={username}
+      />
+
+      {/* Email Input */}
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        placeholderTextColor="#A9A9A9"
+        onChangeText={(text) => setEmail(text)}
+        value={email}
+        keyboardType="email-address"
+      />
+
+      {/* Password Input */}
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        placeholderTextColor="#A9A9A9"
+        secureTextEntry
+        onChangeText={(text) => setPassword(text)}
+        value={password}
+      />
+
+      {/* Sign Up Button */}
+      <TouchableOpacity style={styles.button} onPress={handleSignUp}>
+        <Text style={styles.buttonText}>Sign Up</Text>
+      </TouchableOpacity>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+// Set up the navigation stack
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="SignIn">
+        <Stack.Screen name="SignIn" component={SignInScreen} />
+        <Stack.Screen name="SignUp" component={SignUpScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#212124',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    marginBottom: 10,
+    padding: 10,
+    width: '80%',
+    color: '#fff',
+  },
+  button: {
+    backgroundColor: '#fff',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 10,
+    width: '80%',
+  },
+  buttonText: {
+    color: '#212124',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  linkText: {
+    color: '#fff',
+    marginTop: 15,
   },
 });
